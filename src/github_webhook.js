ab4e6faa81ca89885a719ac48dc9afd379e1212a
Saturnino Abril <saturnino.abril@gmail.com>
2019-10-01 15:36:14 +0800

    fix github webhook listener

--- src/github_webhook.js
+++ src/github_webhook.js
@@ -8,158 +8,14 @@ const GITHUB_ACTION_LABELED = 'labeled';
 const GITHUB_LABEL_QA_REVIEW = '2: QA Review';
 const GITHUB_LABEL_QA_REVIEW_DONE = 'QA Review Done';
 
-module.exports.handler = (event, context, callback) => {
-    var errMsg;
-    const token = process.env.GITHUB_WEBHOOK_SECRET;
-    const headers = event.headers;
-    const sig = headers['X-Hub-Signature'];
-    const githubEvent = headers['X-GitHub-Event'];
-    const id = headers['X-GitHub-Delivery'];
-    const calculatedSig = signRequestBody(token, event.body);
-
-    if (typeof token !== 'string') {
-        errMsg = "Must provide a 'GITHUB_WEBHOOK_SECRET' env variable";
-        return callback(null, {
-            statusCode: 401,
-            headers: {'Content-Type': 'text/plain'},
-            body: errMsg,
-        });
-    }
-
-    if (!sig) {
-        errMsg = 'No X-Hub-Signature found on request';
-        return callback(null, {
-            statusCode: 401,
-            headers: {'Content-Type': 'text/plain'},
-            body: errMsg,
-        });
-    }
-
-    if (!githubEvent) {
-        errMsg = 'No X-Github-Event found on request';
-        return callback(null, {
-            statusCode: 422,
-            headers: {'Content-Type': 'text/plain'},
-            body: errMsg,
-        });
-    }
-
-    if (!id) {
-        errMsg = 'No X-Github-Delivery found on request';
-        return callback(null, {
-            statusCode: 401,
-            headers: {'Content-Type': 'text/plain'},
-            body: errMsg,
-        });
-    }
-
-    if (sig !== calculatedSig) {
-        errMsg =
-            "X-Hub-Signature incorrect. Github webhook token doesn't match";
-        return callback(null, {
-            statusCode: 401,
-            headers: {'Content-Type': 'text/plain'},
-            body: errMsg,
-        });
-    }
-
-    const data = JSON.parse(event.body);
-    const repo = data.repository.full_name;
-    const action = data.action;
-
-    if (
-        action === GITHUB_ACTION_LABELED &&
-        githubEvent === GITHUB_EVENT_PULLS &&
-        process.env.GITHUB_WATCHED_REPOS.split(',').includes(repo)
-    ) {
-        const info = {
-            event: githubEvent,
-            action,
-            repo: `${process.env.GITHUB_WATCHED_REPOS}/${repo}`,
-            sender: data.sender.login,
-            title: data[GITHUB_EVENT_PULLS].title,
-            html_url: data[GITHUB_EVENT_PULLS].html_url,
-        };
-
-        const label = data.label.name;
-
-        if (
-            action === GITHUB_ACTION_LABELED &&
-            [GITHUB_LABEL_QA_REVIEW, GITHUB_LABEL_QA_REVIEW_DONE].includes(
-                label,
-            )
-        ) {
-            const message = labeledTemplate({
-                ...info,
-                label,
-            });
-
-            submitMessage(message);
-            submitToDB(
-                info,
-                action,
-                label,
-                label === GITHUB_LABEL_QA_REVIEW_DONE,
-            );
-        }
-    }
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            input: event,
-        }),
-    };
-
-    return callback(null, response);
-};
-
 // Reuse DB connection
-if (typeof client === 'undefined') {
+if (process.env.DATABASE_CONNECTION_STRING && typeof client === 'undefined') {
     // eslint-disable-line no-use-before-define
     var client = (client = new Client({
         connectionString: process.env.DATABASE_CONNECTION_STRING,
     }));
 
     client.connect();
-}
-
-function signRequestBody(key, body) {
-    return `sha1=${crypto
-        .createHmac('sha1', key)
-        .update(body, 'utf-8')
-        .digest('hex')}`;
-}
-
-function submitMessage(text) {
-    axios({
-        method: 'post',
-        url: process.env.MATTERMOST_INCOMING_WEBHOOK,
-        data: {text},
-    })
-        .then((resp) => {
-            console.log('Successfully sent to Mattermost');
-        })
-        .catch((err) => {
-            // eslint-disable-line handle-callback-err
-            console.log('Failed to sent to Mattermost');
-        });
-}
-
-function labeledTemplate(info) {
-    let emoji = '';
-    let tag = '';
-    if (info.label === GITHUB_LABEL_QA_REVIEW) {
-        tag = '#github_qa_review_request';
-    } else if (info.label === GITHUB_LABEL_QA_REVIEW_DONE) {
-        emoji = ':clap:';
-        tag = '#github_qa_review_done';
-    }
-    return `
-##### ${emoji} [${info.title}](${info.html_url})
-
-[${info.repo}] ${tag} by ${info.sender}
-`;
 }
 
 function submitToDB(info, action, label, isDone) {
@@ -220,6 +76,44 @@ function saveToDB(info, isRequested, isDone) {
     });
 }
 
+function submitMessage(text) {
+    axios({
+        method: 'post',
+        url: process.env.MATTERMOST_INCOMING_WEBHOOK,
+        data: {text},
+    })
+        .then((resp) => {
+            console.log('Successfully sent to Mattermost');
+        })
+        .catch((err) => {
+            // eslint-disable-line handle-callback-err
+            console.log('Failed to sent to Mattermost');
+        });
+}
+
+function labeledTemplate(info) {
+    let emoji = '';
+    let tag = '';
+    if (info.label === GITHUB_LABEL_QA_REVIEW) {
+        tag = '#github_qa_review_request';
+    } else if (info.label === GITHUB_LABEL_QA_REVIEW_DONE) {
+        emoji = ':clap:';
+        tag = '#github_qa_review_done';
+    }
+    return `
+##### ${emoji} [${info.title}](${info.html_url})
+
+[${info.repo}] ${tag} by ${info.sender}
+`;
+}
+
+function signRequestBody(key, body) {
+    return `sha1=${crypto
+        .createHmac('sha1', key)
+        .update(body, 'utf-8')
+        .digest('hex')}`;
+}
+
 function insertQuery(info, isRequested, isDone) {
     const now = new Date().toUTCString();
     const data = [
@@ -255,3 +149,125 @@ function insertQuery(info, isRequested, isDone) {
         data,
     );
 }
+
+module.exports.default = (event, context, callback) => {
+    var errMsg;
+    const token = process.env.GITHUB_WEBHOOK_SECRET;
+    const headers = event.headers;
+    const sig = headers['X-Hub-Signature'];
+    const githubEvent = headers['X-GitHub-Event'];
+    const id = headers['X-GitHub-Delivery'];
+    const calculatedSig = signRequestBody(token, event.body);
+
+    if (typeof token !== 'string') {
+        errMsg = "Must provide a 'GITHUB_WEBHOOK_SECRET' env variable";
+        console.log('NO TOKEN: Must provide a GITHUB_WEBHOOK_SECRET env variable');
+        return callback(null, {
+            statusCode: 401,
+            headers: {'Content-Type': 'text/plain'},
+            body: errMsg,
+        });
+    }
+
+    if (!sig) {
+        errMsg = 'No X-Hub-Signature found on request';
+        console.log('NO SIGNATURE:', errMsg);
+        return callback(null, {
+            statusCode: 401,
+            headers: {'Content-Type': 'text/plain'},
+            body: errMsg,
+        });
+    }
+
+    if (!githubEvent) {
+        errMsg = 'No X-Github-Event found on request';
+        console.log('NO GITHUB EVENT:', errMsg);
+        return callback(null, {
+            statusCode: 422,
+            headers: {'Content-Type': 'text/plain'},
+            body: errMsg,
+        });
+    }
+
+    if (!id) {
+        errMsg = 'No X-Github-Delivery found on request';
+        console.log('NO ID:', errMsg);
+        return callback(null, {
+            statusCode: 401,
+            headers: {'Content-Type': 'text/plain'},
+            body: errMsg,
+        });
+    }
+
+    if (sig !== calculatedSig) {
+        errMsg =
+            "X-Hub-Signature incorrect. Github webhook token doesn't match";
+        console.log('INVALID SIGNATURE:', errMsg);
+        return callback(null, {
+            statusCode: 401,
+            headers: {'Content-Type': 'text/plain'},
+            body: errMsg,
+        });
+    }
+
+    const data = JSON.parse(event.body);
+    const repo = data.repository.full_name;
+    const action = data.action;
+    const repos = process.env.GITHUB_WATCHED_REPOS.split(',').map(
+        (r) => `${process.env.GITHUB_OWNER}/${r}`,
+    );
+
+    console.log('GITHUB_EVENT:', githubEvent);
+    console.log('REPO:', repo);
+    console.log('ACTION:', action);
+
+    if (
+        action === GITHUB_ACTION_LABELED &&
+        githubEvent === GITHUB_EVENT_PULLS &&
+        repos.includes(repo)
+    ) {
+        const info = {
+            event: githubEvent,
+            action,
+            repo,
+            sender: data.sender.login,
+            title: data[GITHUB_EVENT_PULLS].title,
+            html_url: data[GITHUB_EVENT_PULLS].html_url,
+        };
+
+        const label = data.label.name;
+        console.log('LABEL:', label);
+        if (
+            [GITHUB_LABEL_QA_REVIEW, GITHUB_LABEL_QA_REVIEW_DONE].includes(
+                label,
+            )
+        ) {
+            const message = labeledTemplate({
+                ...info,
+                label,
+            });
+
+            submitMessage(message);
+
+            if (process.env.DATABASE_CONNECTION_STRING) {
+                console.log('SUBMIT TO DB');
+                // P2: will remove once DB is setup
+                submitToDB(
+                    info,
+                    action,
+                    label,
+                    label === GITHUB_LABEL_QA_REVIEW_DONE,
+                );
+            }
+        }
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            input: event,
+        }),
+    };
+
+    return callback(null, response);
+};
